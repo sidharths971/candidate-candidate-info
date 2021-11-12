8bd7d6631b207fc55bdef5c1094737d19bedf0c4
sidharths971 <69980505+sidharths971@users.noreply.github.com>
2021-11-12 21:00:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,17 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def hello_world():
+    """
+    Render the page, can upload the file and save in proper directory.
+    """
     if request.method == 'POST':
         image = request.files["image"]
-
+        #--Only csv file can allow to upload.
         if image.filename.split('.')[-1] == 'csv':
             if image.filename not in os.listdir(CandidateConstants.BASE_FILE):
                 image.save(os.path.join(CandidateConstants.BASE_FILE, image.filename))
-
+        
+        #-- Render the template with proper message.
         return render_template('index.html', name=CandidateConstants.MESSAGE)
 
     return render_template('index.html')
@@ -24,6 +28,7 @@ def hello_world():
 
 if __name__ == '__main__':
     logger.debug('server started')
+    #-- For accessing the api import the all file 
     from api import *
 
     app.run(
